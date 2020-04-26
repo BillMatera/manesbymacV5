9b6e6b753f61c1706396d92aa69623f7db2b6de5
William matera <matera.3@osu.edu>
2020-04-26 14:48:23 -0400

    fix services

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,16 +5,16 @@
  */
 
 // You can delete this file if you're not using it
-// const React = require("react")
+const React = require("react")
 
-// export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-//   setHeadComponents([
-//     <script
-//     src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
-//     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
-//     crossorigin="anonymous">
-//     </script>,
-//   ])
-// }
+export const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  setHeadComponents([
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+    crossOrigin="anonymous">
+    </script>,
+  ])
+}
 
 // <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
