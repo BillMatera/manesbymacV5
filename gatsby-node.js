5c673ede69d7d802facf84f26c7b6d59a3c222f1
William matera <matera.3@osu.edu>
2020-04-26 14:34:14 -0400

    fix services

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,20 +6,20 @@
 
 // You can delete this file if you're not using it
 //gatsby-node.js
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: 'jQuery',
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//   if (stage === "build-html") {
+//     actions.setWebpackConfig({
+//       module: {
+//         rules: [
+//           {
+//             test: 'jQuery',
+//             use: loaders.null(),
+//           },
+//         ],
+//       },
+//     })
+//   }
+// }
 
 
 exports.onCreateWebpackConfig = ({
